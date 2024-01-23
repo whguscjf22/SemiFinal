edb8574bdffeda00d57e0faaa0348e1ebb03dd5f
hyunchul <whguscjf22@gmail.com>
2024-01-23 21:02:35 +0900

    페이징 기능개발

--- semiFinal/src/main/java/com/board/mapper/BoardMapper.java
+++ semiFinal/src/main/java/com/board/mapper/BoardMapper.java
@@ -1,13 +1,28 @@
 package com.board.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.board.dto.Board;
+import com.board.dto.PageRequestDTO;
 import com.board.dto.User;
 
 @Mapper
 public interface BoardMapper {
 
-	public User testNotice();
+	public List<Board> getTotalCount();
+
+	public void insertNotice(Board board);
+
+	public List<Board> getBoardListBySearchKeyword(String searchKeyword);
+
+	public List<Board> getNoticeBySearchWithPage(PageRequestDTO pageRequest);
+
+	public int getTotalCount(PageRequestDTO pageRequest);
+
+	public List<Board> getBoardList();
+
 
 
 
