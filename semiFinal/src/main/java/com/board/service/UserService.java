9e8ae40ec20f789fbe8e3868bbaa2b2096a15e2e
JJIN\HJ <pizza.jinzza@gmail.com>
2024-01-23 22:45:18 +0900

    로그인, 회원가입 기능 (login.jsp, joinUser.jsp 는 확인용으로 작성)

--- semiFinal/src/main/java/com/board/service/UserService.java
+++ semiFinal/src/main/java/com/board/service/UserService.java
@@ -1,5 +1,41 @@
 package com.board.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.board.dto.User;
+import com.board.mapper.UserMapper;
+
+@Service
 public class UserService {
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	public User getUserByUserIdAndPassword(String userId, String password) {
+		User user = null;
+		user = userMapper.getUserByUserIdAndPassword(userId, password);
+		
+		return user;
+	}
+	
+	public User getUserByUserId(String userId) {
+		User user = null;
+		user = userMapper.getUserByUserId(userId);
+		return user;
+	}
+
+	public boolean joinUser(String userId, String userName,String password) {
+		boolean result = false;
+		
+		int res = userMapper.joinUser(userId,userName,password);
+		if (res != 0 ) {
+			result = true;
+		}else {
+			new Exception("회원가입 실패");
+		}
+		return result;
+	}
+
 
 }
--- semiFinal/src/main/webapp/WEB-INF/views/main.jsp
+++ semiFinal/src/main/webapp/WEB-INF/views/main.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<hr>
 </body>
 </html>
